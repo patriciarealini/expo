913f032ef30dd2f8c9ecd95043e4f0ad63923555
patriciarealini <hello@patriciarealini.com>
2016-04-21 00:58:02 -0700

    the gulped version of my work that is built by my npm scripts. Should I gitignore this in the future?

---
+++ tmp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Modal} from "./components/index.jsx";
+
+ReactDOM.render(
+  <div>
+    <h1>Ando Expo</h1>
+    <Modal />
+  </div>
+  // document.getElementById('example')
+);
