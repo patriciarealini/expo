d7a43f5a1fa17653f8d49d12284c96071dd5000a
patriciarealini <hello@patriciarealini.com>
2016-04-21 00:55:05 -0700

    Concat css files. Pull html file. Gulp it down and send it to /tmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
-var gulp = require('gulp');
-var gulpConcat = require('gulpConcat');
+const gulp = require('gulp');
+const gulpConcat = require('gulp-concat');
+const gulpSass = require('gulp-sass');
+
+const HTMLS = ["ui/index.html"];
+const STYLES = [
+  "node_modules/normalize.css/normalize.css",
+  "ui/index.css",
+  "ui/modal.scss"
+];
+const STYLE = "index.css";
+const DESTINATION = "tmp/";
 
 gulp.task("styles", function() {
   return gulp.src(STYLES)
   .pipe(gulpConcat(STYLE))
+  .pipe(gulpSass().on('error', gulpSass.logError))
   .pipe(gulp.dest(DESTINATION))
 });
 
-gulp.task('default', function() {
-
+gulp.task("htmls", function() {
+  return gulp.src(HTMLS)
+  .pipe(gulp.dest(DESTINATION))
 });
+
+gulp.task('default', ["styles", "htmls"]);
