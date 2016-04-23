928bbc4e69472b793c01e096f88baf0f6af93c11
patriciarealini <hello@patriciarealini.com>
2016-04-23 12:46:32 -0700

    Install and set up mocha chai and babel-register so i can write tests and run them.

---
+++ test/test.js
@@ -0,0 +1,15 @@
+//test.js
+var Chai = require("chai");
+
+var assert = Chai.assert;
+var expect = Chai.expect;
+var should = Chai.should();
+
+// describe('Array', function() {
+//   describe('#indexOf()', function () {
+//     it('should return -1 when the value is not present', function () {
+//       assert.equal(-1, [1,2,3].indexOf(5));
+//       assert.equal(-1, [1,2,3].indexOf(0));
+//     });
+//   });
+// });
