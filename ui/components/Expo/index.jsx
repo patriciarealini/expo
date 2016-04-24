e937717f253db3d157255a160d6012033a303e49
patriciarealini <hello@patriciarealini.com>
2016-04-24 14:09:12 -0700

    plug in ramda & connect to access the store with redux. pass in state as props.

--- ui/components/Expo/index.jsx
+++ ui/components/Expo/index.jsx
@@ -1,38 +1,67 @@
-import React, {Component} from "react"
+import React, {Component, PropTypes} from "react"
+import {connect} from "react-redux"
+import {filter, values, whereEq} from "ramda"
 import Radium from "radium"
 
 import Ticket from "../Ticket/index.jsx"
 
-@Radium
-class Expo extends Component {
-
-  renderTicket () {
-
-    return (
-      <Ticket
-        courierETA={new Date()}
-        courierName={"Mr. Messenger Bag"}
-        customerAddress={"123 Prince St., New York, NY 10001"}
-        customerName={"Prince"}
-        orderNumber={1} />
-    )
-  }
-
-  render () {
-
-    return (
-      <div>
-        <section style={styles.expo}>
-          {this.renderTicket()}
-        </section>
-      </div>
-    )
+const styles = {
+  expo: {
+    display: "flex"
   }
 }
 
-const styles = {
-  expo: {
+@Radium
+@connect((state) => {
+  const isCompleted = whereEq({completed: true})
+  const inTheQueue = whereEq({completed: false})
+  const onlyQueue = filter(inTheQueue)
+  const onlyCompleted = filter(isCompleted)
 
+  return {
+    tickets: onlyQueue(values(state.tickets)),
+    ticketsCompleted: onlyCompleted(values(state.tickets))
+  }
+})
+class Expo extends Component {
+
+  static propTypes = {
+    tickets: PropTypes.array.isRequired,
+    ticketsCompleted: PropTypes.array.isRequired
+  }
+
+  static defaultProps = {
+    tickets: [],
+    ticketsCompleted: []
+  }
+
+  renderTicket (ticket) {
+    return (
+      <Ticket
+        key={ticket.orderNumber}
+        courierETA={ticket.courierETA}
+        courierName={ticket.courierName}
+        customerAddress={ticket.customerAddress}
+        customerName={ticket.customerName}
+        orderNumber={ticket.orderNumber} />
+    )
+  }
+
+  renderTickets (tickets) {
+    return tickets.map(this.renderTicket)
+  }
+
+  render () {
+    return (
+      <div>
+        <section style={styles.expo}>
+          {this.renderTickets(this.props.tickets)}
+        </section>
+        <section style={styles.expo}>
+          {this.renderTickets(this.props.ticketsCompleted)}
+        </section>
+      </div>
+    )
   }
 }
 
