59f8c43a6a90d78f6a90a3ff695c9187e2dc9efe
patriciarealini <hello@patriciarealini.com>
2016-04-24 15:08:33 -0700

    Add completed and dispatch to the props

--- ui/components/Ticket/index.jsx
+++ ui/components/Ticket/index.jsx
@@ -1,6 +1,8 @@
 import React, {Component, PropTypes} from "react"
 import Radium from "radium"
 
+import {markTicketCompleted} from "../../actions/index.js"
+
 const styles = {
   ticket: {
     flex: 1,
@@ -13,22 +15,29 @@ const styles = {
 class Ticket extends Component {
 
   static propTypes = {
+    completed: PropTypes.bool.isRequired,
     courierETA: PropTypes.instanceOf(Date).isRequired,
     courierName: PropTypes.string.isRequired,
     customerAddress: PropTypes.string.isRequired,
     customerName: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired,
     orderNumber: PropTypes.number.isRequired
+  }
+
+  onClickCompleted () {
+    this.props.dispatch(markTicketCompleted(this.props.orderNumber))
   }
 
   render () {
     return (
       <div>
         <div style={styles.ticket}>
-          <h2>{this.props.orderNumber}</h2>
-          <p>{this.props.customerName}</p>
-          <p>{this.props.customerAddress}</p>
-          <p>{this.props.courierName}</p>
-          <p>{this.props.courierETA.toISOString()}</p>
+          <h2>Order Number: {this.props.orderNumber}</h2>
+          <p>Customer: {this.props.customerName}</p>
+          <p>Address: {this.props.customerAddress}</p>
+          <p>Courier: {this.props.courierName}</p>
+          <p>Courier ETA: {this.props.courierETA.toISOString()}</p>
+          <button onClick={this.onClickCompleted}>Pick Up(add if statement)</button>
         </div>
       </div>
     )
