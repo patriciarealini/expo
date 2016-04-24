3343cacbbbabab1288322244e9cad07de5d92992
patriciarealini <hello@patriciarealini.com>
2016-04-24 14:02:35 -0700

    write gulp task to watch for build changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
-const gulp = require('gulp');
-const gulpConcat = require('gulp-concat');
-const gulpSass = require('gulp-sass');
+const gulp = require('gulp')
+const gulpConcat = require('gulp-concat')
+const gulpSass = require('gulp-sass')
 
-const HTMLS = ["ui/index.html"];
+const HTMLS = ["ui/index.html"]
 const STYLES = [
   "node_modules/normalize.css/normalize.css",
   "ui/index.scss"
-];
-const STYLE = "index.css";
-const DESTINATION = "tmp/";
+]
+const STYLE = "index.css"
+const DESTINATION = "tmp/"
 
 gulp.task("styles", function() {
   return gulp.src(STYLES)
   .pipe(gulpConcat(STYLE))
   .pipe(gulpSass().on('error', gulpSass.logError))
   .pipe(gulp.dest(DESTINATION))
-});
+})
 
 gulp.task("htmls", function() {
   return gulp.src(HTMLS)
   .pipe(gulp.dest(DESTINATION))
-});
+})
 
-gulp.task('default', ["styles", "htmls"]);
+gulp.task("watch", ["styles", "htmls"], () => {
+  gulp.watch(STYLES, ["styles"])
+  gulp.watch(HTMLS, ["htmls"])
+})
+
+gulp.task('default', ["styles", "htmls"])
