36933e90471b40adf653d6576709b472afdc0da1
patriciarealini <hello@patriciarealini.com>
2016-04-24 14:04:39 -0700

    Set up Provider and createStore for redux

--- ui/index.jsx
+++ ui/index.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import {Provider} from "react-redux"
+import {createStore} from "redux"
 
-import {Header, Expo} from "./components/index.jsx";
+import {getAllTickets} from "./actions/index.js"
+import reducers from "./reducers/index.js"
+import {Header, Expo} from "./components/index.jsx"
+
+const store = createStore(reducers)
+
+store.dispatch(getAllTickets())
 
 ReactDOM.render(
   <div>
     <Header />
-    <Expo />
+    <Provider store={store}>
+      <Expo />
+    </Provider>
   </div>,
-  document.getElementById('ando')
-);
+  document.getElementById("ando")
+)
