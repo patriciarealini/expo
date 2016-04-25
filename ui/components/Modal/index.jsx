5c520179dd9065b661fccad11dd005396e450d3f
patriciarealini <hello@patriciarealini.com>
2016-04-25 12:05:50 -0700

    set up Modal to handle changing state.help.open to false when the No button is clicked

--- ui/components/Modal/index.jsx
+++ ui/components/Modal/index.jsx
@@ -1,24 +1,69 @@
-import React, {Component} from "react"
+import React, {Component, PropTypes} from "react"
 import Radium from "radium"
 
+import {closeModal} from "../../actions/index.js"
+
 const styles = {
-  button: {}
+  modal: {
+    display: "block",
+    position: "absolute",
+    width: "100vw",
+    height: "100vh",
+    background: "#5d5d5d"
+  },
+  selection: {
+    display: "flex",
+    jusitfyContent: "center"
+  },
+  button: {
+    flex: 1
+  }
+}
+
+const onClickChangeView = (action, dispatch) => () => {
+  dispatch(action())
 }
 
 @Radium
+// @connect((state) => {
+//   switch (state.help.open) {
+//     case "false": {
+//       return {
+//         open: state.help.open === false
+//       }
+//     }
+//     case "true":
+//     default: {
+//       return {
+//         open: state.help.open === true
+//       }
+//     }
+//   }
+// })
 class Modal extends Component {
 
-  render () {
-    return (
-      <div>
-        <h1>Help!</h1>
-        <p>Are you sure you want to close the kitchen?</p>
-        <div>
-          <button style={styles.button}>Yes</button>
-          <button style={styles.button}>No</button>
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired
+  }
+
+  renderModal (open) {
+    if (open === true) {
+      return (
+        <div style={styles.modal}>
+          <h1>Help!</h1>
+          <p>Are you sure you want to close the kitchen?</p>
+          <div style={styles.selection}>
+            <button style={styles.button}>Yes</button>
+            <button onClick={onClickChangeView(closeModal, this.props.dispatch)} style={styles.button}>No</button>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
+  }
+
+  render () {
+    return renderModal(this.props.open)
   }
 }
 
