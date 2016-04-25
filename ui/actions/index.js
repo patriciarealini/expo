a600c9d59fadea5036343632b1d574b9d35f7c2e
patriciarealini <hello@patriciarealini.com>
2016-04-24 22:52:35 -0700

    create action types for the session view & change the values of the preset state to Chefs & their restaurants in NY with famous NY food writers as their couriers.

--- ui/actions/index.js
+++ ui/actions/index.js
@@ -3,36 +3,84 @@ export function getAllTickets () {
     type: "RECEIVE_TICKETS",
     tickets: {
       1: {
-        completed: true,
+        completed: false,
         courierETA: new Date(),
-        courierName: "Courier 1",
-        customerAddress: "Customer Address 1",
-        customerName: "Customer Name 1",
+        courierName: "Ruth Reichl",
+        customerAddress: "80 Spring St New York, NY 10012",
+        customerName: "Keith McNally",
         orderNumber: 1
       },
       2: {
         completed: false,
         courierETA: new Date(),
-        courierName: "Courier 2",
-        customerAddress: "Customer Address 2",
-        customerName: "Customer Name 2",
+        courierName: "Pete Wells",
+        customerAddress: "171 East Broadway, New York, NY 10002",
+        customerName: "Danny Bowien",
         orderNumber: 2
       },
       3: {
         completed: false,
         courierETA: new Date(),
-        courierName: "Courier 3",
-        customerAddress: "Customer Address 3",
-        customerName: "Customer Name 3",
+        courierName: "John L. Hess",
+        customerAddress: "11 Madison Ave, New York, NY 10010",
+        customerName: "Daniel Humm",
         orderNumber: 3
       },
       4: {
         completed: false,
         courierETA: new Date(),
-        courierName: "Courier 4",
-        customerAddress: "Customer Address 4",
-        customerName: "Customer Name 4",
+        courierName: "Bryan Miller",
+        customerAddress: "10 Columbus Cir, New York, NY 10019",
+        customerName: "Thomas Keller",
         orderNumber: 4
+      },
+      5: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "Peter Meehan",
+        customerAddress: "175 Avenue B, New York, NY 10009",
+        customerName: "Ruth Krishna",
+        orderNumber: 5
+      },
+      6: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "Craig Claiborne",
+        customerAddress: "179 E Houston St, New York, NY 10002",
+        customerName: "Joel Russ",
+        orderNumber: 6
+      },
+      7: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "Frank Bruni",
+        customerAddress: "54 East 1st St, New York, NY 10003",
+        customerName: "Gabrielle Hamilton",
+        orderNumber: 7
+      },
+      8: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "Mimi Sheraton",
+        customerAddress: "60 E 65th St, New York, NY 10065",
+        customerName: "Daniel Boulud",
+        orderNumber: 8
+      },
+      9: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "William Grimes",
+        customerAddress: "43 E 19th St, New York, NY 10003",
+        customerName: "Tom Colicchio",
+        orderNumber: 9
+      },
+      10: {
+        completed: false,
+        courierETA: new Date(),
+        courierName: "Sam Sifton",
+        customerAddress: "382 Metropolitan Ave, Brooklyn, NY 11211",
+        customerName: "Christina Tosi",
+        orderNumber: 10
       }
     }
   }
@@ -41,5 +89,15 @@ export function markTicketCompleted (id) {
   return {
     type: "MARK_TICKET_COMPLETED",
     id
+  }
+}
+export function viewCompleted () {
+  return {
+    type: "VIEW_COMPLETED"
+  }
+}
+export function viewQueued () {
+  return {
+    type: "VIEW_QUEUED"
   }
 }
