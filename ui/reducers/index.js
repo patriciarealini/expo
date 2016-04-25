3d2032dcb5dad527a65fec763a5322f0d2ac2f0c
patriciarealini <hello@patriciarealini.com>
2016-04-25 03:30:44 -0700

    create a new reducer, session, to handle state changes when the view is toggled from the header. Oh and i guess i didn't commit my reducer for marking a ticket as completed.

--- ui/reducers/index.js
+++ ui/reducers/index.js
@@ -10,6 +10,34 @@ function tickets (state = initalState, action) {
         ...action.tickets
       }
     }
+    case "MARK_TICKET_COMPLETED": {
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          completed: true
+        }
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+function session (state = initalState, action) {
+  switch (action.type) {
+    case "VIEW_COMPLETED": {
+      return {
+        ...state,
+        view: "completed"
+      }
+    }
+    case "VIEW_QUEUED": {
+      return {
+        ...state,
+        view: "queued"
+      }
+    }
     default: {
       return state
     }
@@ -17,5 +45,6 @@ function tickets (state = initalState, action) {
 }
 
 export default combineReducers({
-  tickets
+  tickets,
+  session
 })
