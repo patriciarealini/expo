5a78da216076625a6dfc09de1fd3e5a5c137529f
patriciarealini <hello@patriciarealini.com>
2016-04-24 22:07:01 -0700

    create toggleView button to trigger redux action that will display completed tickets or queued tickets

--- ui/components/Header/index.jsx
+++ ui/components/Header/index.jsx
@@ -1,27 +1,54 @@
-import React, {Component} from "react";
-import Radium from "radium";
+import React, {Component, PropTypes} from "react"
+import {connect} from "react-redux"
+import Radium from "radium"
 
-import Modal from "../Modal/index.jsx";
-
-@Radium
-class Header extends Component {
-
-  render () {
-
-    return (
-      <div style={styles.header}>
-        <h1>Ando!</h1>
-        <Modal />
-      </div>
-    );
-  }
-};
+import {viewCompleted, viewQueued} from "../../actions/index.js"
+import Modal from "../Modal/index.jsx"
 
 const styles = {
   header: {
     backgroundColor: "Tomato",
     border: "3px solid Firebrick"
   }
-};
+}
 
-export default Header;
+const onClickChangeView = (action, dispatch) => () => {
+  dispatch(action())
+}
+
+const toggleView = (view, dispatch) => {
+  if (view === "completed") {
+    return (
+      <button onClick={onClickChangeView(viewQueued, dispatch)}>View Queue</button>
+    )
+  }
+  return (
+    <button onClick={onClickChangeView(viewCompleted, dispatch)}>View Completed Tickets</button>
+  )
+}
+
+@Radium
+@connect((state) => {
+  return {
+    view: state.session.view
+  }
+})
+class Header extends Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    view: PropTypes.string
+  }
+
+  render () {
+    return (
+      <div style={styles.header}>
+        <h1>Ando!</h1>
+        {toggleView(this.props.view, this.props.dispatch)}
+        <Modal />
+      </div>
+    )
+  }
+}
+
+export default Header
