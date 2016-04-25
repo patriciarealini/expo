06ebb1e33fc00e1af300618fba49f266fc7e5951
patriciarealini <hello@patriciarealini.com>
2016-04-25 10:10:01 -0700

    create actions for the opening & closing of the modal

--- ui/actions/index.js
+++ ui/actions/index.js
@@ -101,3 +101,13 @@ export function viewQueued () {
     type: "VIEW_QUEUED"
   }
 }
+export function openModal () {
+  return {
+    type: "OPEN_MODAL"
+  }
+}
+export function closeModal () {
+  return {
+    type: "CLOSE_MODAL"
+  }
+}
