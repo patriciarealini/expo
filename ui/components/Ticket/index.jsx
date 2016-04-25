3b50213f9791743b2953859a99b190b960a2307d
patriciarealini <hello@patriciarealini.com>
2016-04-25 02:56:29 -0700

    looking stylish

--- ui/components/Ticket/index.jsx
+++ ui/components/Ticket/index.jsx
@@ -4,15 +4,49 @@ import Radium from "radium"
 import {markTicketCompleted} from "../../actions/index.js"
 
 const styles = {
-  pickUpButton: {
-
+  ticketContainer: {
+    marginTop: 115
   },
   ticket: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
-    width: "200px",
-    border: "1px solid black"
+    alignItems: "flex-start",
+    width: 350,
+    backgroundColor: "#FFDF0F",
+    border: "4px white",
+    borderStyle: "dotted solid",
+    margin: "10px",
+    paddingTop: 30
+  },
+  order: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    width: "95%",
+    margin: "2.5%"
+  },
+  info: {
+    width: 100,
+    color: "#5d5d5d",
+    fontFamily: "'Andale Mono', AndaleMono, monospace",
+    borderBottom: "2px solid #FFFFFF",
+    paddingLeft: 5
+  },
+  details: {
+    flex: 1,
+    color: "#5d5d5d",
+    fontFamily: "'Andale Mono', AndaleMono, monospace",
+    borderLeft: "2px solid #FFFFFF",
+    borderBottom: "2px solid #FFFFFF",
+    paddingLeft: 5
+  },
+  pickUpButton: {
+    color: "#FFFFFF",
+    fontSize: "1.7em",
+    width: "95%",
+    height: 60,
+    alignSelf: "flex-end",
+    margin: "2.5%"
   }
 }
 
@@ -46,13 +80,28 @@ class Ticket extends Component {
 
   render () {
     return (
-      <div>
+      <div style={styles.ticketContainer}>
         <div style={styles.ticket}>
-          <h2>Order Number: {this.props.orderNumber}</h2>
-          <p>Customer: {this.props.customerName}</p>
-          <p>Address: {this.props.customerAddress}</p>
-          <p>Courier: {this.props.courierName}</p>
-          <p>Courier ETA: {this.props.courierETA.toISOString()}</p>
+          <ul style={styles.order}>
+            <li style={styles.info}>Order #</li>
+            <li style={styles.details}>{this.props.orderNumber}</li>
+          </ul>
+          <ul style={styles.order}>
+            <li style={styles.info}>Customer</li>
+            <li style={styles.details}>{this.props.customerName}</li>
+          </ul>
+          <ul style={styles.order}>
+            <li style={styles.info}>Customer Address</li>
+            <li style={styles.details}>{this.props.customerAddress}</li>
+          </ul>
+          <ul style={styles.order}>
+            <li style={styles.info}>Courier</li>
+            <li style={styles.details}>{this.props.courierName}</li>
+          </ul>
+          <ul style={styles.order}>
+            <li style={styles.info}>Courier ETA</li>
+            <li style={styles.details}>{this.props.courierETA.toISOString()}</li>
+          </ul>
           {this.renderPickUpButton()}
         </div>
       </div>
