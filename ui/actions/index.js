95e9c4ceca4976c79e7c203ff405700ac3066f15
patriciarealini <hello@patriciarealini.com>
2016-05-07 17:25:27 -0700

    move new ticket creation into /actions, to be triggered by state.session.off and determined by fetchTicket. refactor new Date to newETA & use moment to format.

--- ui/actions/index.js
+++ ui/actions/index.js
@@ -1,7 +1,9 @@
-import {date} from "faker"
+import {keys} from "ramda"
+import {name, address, date} from "faker"
 import moment from "moment"
 
-const randomMinutes = () => Math.floor(Math.random() * 59 + 1)
+const randomMinutes = () => Math.floor(Math.random() * (59) + 1)
+const newETA = () => date.between(new Date(), moment().add(randomMinutes(), "m"))
 
 export function getAllTickets() {
   return {
@@ -9,85 +11,124 @@ export function getAllTickets() {
     tickets: {
       1: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Ruth Reichl",
         customerAddress: "80 Spring St., New York, NY 10012",
         customerName: "Keith McNally",
-        orderNumber: 1
+        orderNumber: 1,
+        updatedAt: new Date()
       },
       2: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Pete Wells",
         customerAddress: "171 East Broadway, New York, NY 10002",
         customerName: "Danny Bowien",
-        orderNumber: 2
+        orderNumber: 2,
+        updatedAt: new Date()
       },
       3: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Bryan Miller",
         customerAddress: "10 Columbus Cir., New York, NY 10019",
         customerName: "Thomas Keller",
-        orderNumber: 3
+        orderNumber: 3,
+        updatedAt: new Date()
       },
       4: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Craig Claiborne",
         customerAddress: "179 E Houston St., New York, NY 10002",
         customerName: "Joel Russ",
-        orderNumber: 4
+        orderNumber: 4,
+        updatedAt: new Date()
       },
       5: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Peter Meehan",
         customerAddress: "175 Avenue B, New York, NY 10009",
         customerName: "Ruth Krishna",
-        orderNumber: 5
+        orderNumber: 5,
+        updatedAt: new Date()
       },
       6: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "John L. Hess",
         customerAddress: "11 Madison Ave., New York, NY 10010",
         customerName: "Daniel Humm",
-        orderNumber: 6
+        orderNumber: 6,
+        updatedAt: new Date()
       },
       7: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Frank Bruni",
         customerAddress: "54 East 1st St., New York, NY 10003",
         customerName: "Gabrielle Hamilton",
-        orderNumber: 7
+        orderNumber: 7,
+        updatedAt: new Date()
       },
       8: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Mimi Sheraton",
         customerAddress: "60 E 65th St., New York, NY 10065",
         customerName: "Daniel Boulud",
-        orderNumber: 8
+        orderNumber: 8,
+        updatedAt: new Date()
       },
       9: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "William Grimes",
         customerAddress: "43 E 19th St., New York, NY 10003",
         customerName: "Tom Colicchio",
-        orderNumber: 9
+        orderNumber: 9,
+        updatedAt: new Date()
       },
       10: {
         completed: false,
-        courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
+        courierETA: newETA(),
         courierName: "Sam Sifton",
         customerAddress: "382 Metropolitan Ave., Brooklyn, NY 11211",
         customerName: "Christina Tosi",
-        orderNumber: 10
+        orderNumber: 10,
+        updatedAt: new Date()
       }
     }
+  }
+}
+export function newTicket (ticket) {
+  return {
+    type: "NEW_TICKET",
+    ticket
+  }
+}
+export function noTicket () {
+  return {
+    type: "NO_TICKET"
+  }
+}
+export function fetchTicket () {
+  return (dispatch, getState) => {
+    if (getState().session.off) {
+      dispatch(noTicket())
+    }
+    const id = keys(getState().tickets).length + 1
+    const ticket = {
+      completed: false,
+      courierETA: newETA(),
+      courierName: name.findName(),
+      customerAddress: `${address.streetAddress()}, New York, NY 10002`,
+      customerName: name.findName(),
+      orderNumber: id,
+      updatedAt: new Date()
+    }
+    dispatch(newTicket(ticket))
   }
 }
 export function markTicketCompleted (id) {
@@ -114,10 +155,5 @@ export function openModal () {
 export function closeModal () {
   return {
     type: "CLOSE_MODAL"
-  }
-}
-export function newTicket () {
-  return {
-    type: "NEW_TICKET"
   }
 }
