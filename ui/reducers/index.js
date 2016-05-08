0d94d2a734bde12cca4894fc08c740fa960d43e9
patriciarealini <hello@patriciarealini.com>
2016-05-07 17:20:30 -0700

    add updatedAt for shouldComponentUpdate function in Tickets & abstract out new Ticket creation to /actions

--- ui/reducers/index.js
+++ ui/reducers/index.js
@@ -1,10 +1,6 @@
 import {combineReducers} from "redux"
-import {keys} from "ramda"
-import {name, address, date} from "faker"
-import moment from "moment"
 
 const initialState = {}
-const randomMinutes = () => Math.floor(Math.random() * 59 + 1)
 
 function tickets (state = initialState, action) {
   switch (action.type) {
@@ -19,23 +15,15 @@ function tickets (state = initialState, action) {
         ...state,
         [action.id]: {
           ...state[action.id],
+          updatedAt: new Date(),
           completed: true
         }
       }
     }
     case "NEW_TICKET": {
-      const id = keys(tickets).length + 1
-
       return {
         ...state,
-        [id]: {
-          completed: false,
-          courierETA: date.between(new Date(), moment().add(randomMinutes(), "minutes")),
-          courierName: name.findName(),
-          customerAddress: address.streetAddress(),
-          customerName: name.findName(),
-          orderNumber: id
-        }
+        [action.ticket.orderNumber]: action.ticket
       }
     }
     default: {
