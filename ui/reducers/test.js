1c97eb3c4cc60611f3031523172d84f6106c3e01
patriciarealini <hello@patriciarealini.com>
2016-05-08 14:37:36 -0700

    close modal test

--- ui/reducers/test.js
+++ ui/reducers/test.js
@@ -1,7 +1,25 @@
-const Chai = require("chai")
+import {describe, it} from "mocha"
+import {expect} from "chai"
+import help from "./index.js"
 
-import reducers from "./index.js"
+describe("ui/reducers", () => {
+  describe("help()", () => {
+    describe("when CLOSE_MODAL is called", () => {
+      const action = {
+        type: "CLOSE_MODAL"
+      }
 
-const assert = Chai.assert
-const expect = Chai.expect
-const should = Chai.should()
+      describe("when already open", () => {
+        const state = {
+          open: true
+        }
+
+        it("closes the modal", () => {
+          expect(help(state, action)).to.have.property("help").that.deep.equals({
+            open: false
+          })
+        })
+      })
+    })
+  })
+})
